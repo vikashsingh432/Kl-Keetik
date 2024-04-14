02ec918cc924bd8dd70cf52b6cf7ecc66ce20c12
vikashsingh432 <132069179+vikashsingh432@users.noreply.github.com>
2024-04-14 13:59:22 +0530

    Send.php
    Signed-off-by: vikashsingh432 <132069179+vikashsingh432@users.noreply.github.com>

--- Send.php
+++ Send.php
@@ -6,9 +6,9 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
